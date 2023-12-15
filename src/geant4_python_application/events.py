cfa0e4da6a4210fae34f796d6feca6a439c7ecd7
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-12-15 01:10:12 -0600

    compute hits

--- src/geant4_python_application/events.py
+++ src/geant4_python_application/events.py
@@ -16,6 +16,13 @@ class EventArray(ak.Array):
         # collapse to an array of scalars (one per event)
         return ak.flatten(ak.sum(energy, axis=-1))
 
+    def hits(self, volume) -> ak.Array:
+        hits = ak.flatten(self.track.step, axis=-1)
+        hits = hits[hits.volume == volume][hits.energy > 0]
+        # only keep x,y,z, time, and energy
+        hits = hits[["position_x", "position_y", "position_z", "time", "energy"]]
+        return hits
+
 
 ak.behavior["event"] = EventRecord
 ak.behavior["*", "event"] = EventArray
