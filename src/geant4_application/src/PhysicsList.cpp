2d4a4f880bf9b8fd211c2bf29ba47d8f5f215f8c
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-12-15 21:04:32 -0600

    working on physics list

--- src/geant4_application/src/PhysicsList.cpp
+++ src/geant4_application/src/PhysicsList.cpp
@@ -1,17 +1,29 @@
 
 #include "geant4_application/PhysicsList.h"
 
+#include <G4Box.hh>
 #include <G4DecayPhysics.hh>
 #include <G4EmExtraPhysics.hh>
 #include <G4EmStandardPhysics.hh>
 #include <G4EmStandardPhysics_option4.hh>
 #include <G4HadronElasticPhysics.hh>
+#include <G4HadronElasticPhysicsHP.hh>
 #include <G4HadronPhysicsFTFP_BERT.hh>
+#include <G4HadronPhysicsQGSP_BIC_HP.hh>
+#include <G4HadronicProcessStore.hh>
 #include <G4IonPhysics.hh>
+#include <G4Neutron.hh>
 #include <G4NeutronTrackingCut.hh>
+#include <G4NistManager.hh>
+#include <G4PVPlacement.hh>
+#include <G4ProcessTable.hh>
+#include <G4ProcessTableMessenger.hh>
 #include <G4RadioactiveDecayPhysics.hh>
 #include <G4StoppingPhysics.hh>
+#include <G4SystemOfUnits.hh>
 #include <G4VModularPhysicsList.hh>
+#include <G4VUserDetectorConstruction.hh>
+#include <QBBC.hh>
 
 using namespace geant4_app;
 
@@ -41,4 +53,16 @@ PhysicsList::PhysicsList() : G4VModularPhysicsList() {
 
     // Neutron tracking cut
     RegisterPhysics(new G4NeutronTrackingCut());
+}
+
+std::set<std::string> PhysicsList::GetPhysicsListNames() {
+    std::set<std::string> names;
+    // TODO
+    return names;
+}
+
+std::set<std::string> PhysicsList::GetProcessNames() {
+    std::set<std::string> names;
+    // TODO
+    return names;
 }
