3f93ccbba8706ccdd90c1acd9979bfffbdb0a2e0
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-02-06 12:05:34 +0100

    fix versioning in cmake/python

--- src/geant4_python_application/__init__.py
+++ src/geant4_python_application/__init__.py
@@ -1,23 +1,23 @@
 from __future__ import annotations
 
 from geant4_python_application._geant4_application import (
-    __doc__,
-    __version__,
     awkward_version,
     geant4_version,
     pybind11_version,
 )
+from geant4_python_application._version import version, version_tuple
 from geant4_python_application.application import Application
 from geant4_python_application.detector import Detector
 from geant4_python_application.files.datasets import data_directory, install_datasets
 from geant4_python_application.files.directories import application_directory
 from geant4_python_application.gdml import basic_gdml
 
-version = __version__
+__version__ = version
+__version__tuple__ = version_tuple
 
 __all__ = [
-    "__doc__",
     "__version__",
+    "__version__tuple__",
     "geant4_version",
     "awkward_version",
     "pybind11_version",
