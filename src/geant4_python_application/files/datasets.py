1b65db64e23b0272c5342c9e87002f6bd435a08b
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-10-06 03:54:09 +0200

    restore checksum, fix bad checksum

--- src/geant4_python_application/files/datasets.py
+++ src/geant4_python_application/files/datasets.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import concurrent.futures
+import hashlib
 import os
 import shutil
 import tarfile
@@ -16,6 +17,9 @@ import geant4_python_application
 url = "https://cern.ch/geant4-data/datasets"
 
 
+# It is discouraged to use the python package directory to store data
+# data_dir = os.path.join(os.path.dirname(__file__), "geant4/data")
+# another idea is to use 'platformdirs' to store data in a platform-specific location
 def data_directory() -> str:
     return os.path.join(
         geant4_python_application.application_directory(),
@@ -26,46 +30,87 @@ def data_directory() -> str:
     )
 
 
-Dataset = namedtuple("Dataset", ["name", "version", "filename", "env"])
+# the datasets versions should be updated with each Geant4 version (remember to update the checksum too!)
+# https://geant4.web.cern.ch/download/11.2.2.html#datasets
+Dataset = namedtuple("Dataset", ["name", "version", "filename", "env", "md5sum"])
 
 datasets = (
-    Dataset(name="G4NDL", version="4.7.1", filename="G4NDL", env="G4NEUTRONHPDATA"),
-    Dataset(name="G4EMLOW", version="8.5", filename="G4EMLOW", env="G4LEDATA"),
+    Dataset(
+        name="G4NDL",
+        version="4.7.1",
+        filename="G4NDL",
+        env="G4NEUTRONHPDATA",
+        md5sum="54f0ed3995856f02433d42ec96d70bc6",
+    ),
+    Dataset(
+        name="G4EMLOW",
+        version="8.5",
+        filename="G4EMLOW",
+        env="G4LEDATA",
+        md5sum="146d0625d8d39f294056e1618271bc46",
+    ),
     Dataset(
         name="PhotonEvaporation",
         version="5.7",
         filename="G4PhotonEvaporation",
         env="G4LEVELGAMMADATA",
+        md5sum="81ff27deb23af4aa225423e6b3a06b39",
     ),
     Dataset(
         name="RadioactiveDecay",
         version="5.6",
         filename="G4RadioactiveDecay",
         env="G4RADIOACTIVEDATA",
+        md5sum="acc1dbeb87b6b708b2874ced729a3a8f",
     ),
     Dataset(
         name="G4PARTICLEXS",
         version="4.0",
         filename="G4PARTICLEXS",
         env="G4PARTICLEXSDATA",
+        md5sum="d82a4d171d50f55864e28b6cd6f433c0",
     ),
-    Dataset(name="G4PII", version="1.3", filename="G4PII", env="G4PIIDATA"),
+    Dataset(
+        name="G4PII",
+        version="1.3",
+        filename="G4PII",
+        env="G4PIIDATA",
+        md5sum="05f2471dbcdf1a2b17cbff84e8e83b37",
+    ),
     Dataset(
         name="RealSurface",
         version="2.2",
         filename="G4RealSurface",
         env="G4REALSURFACEDATA",
+        md5sum="ea8f1cfa8d8aafd64b71fb30b3e8a6d9",
     ),
     Dataset(
-        name="G4SAIDDATA", version="2.0", filename="G4SAIDDATA", env="G4SAIDXSDATA"
+        name="G4SAIDDATA",
+        version="2.0",
+        filename="G4SAIDDATA",
+        env="G4SAIDXSDATA",
+        md5sum="d5d4e9541120c274aeed038c621d39da",
     ),
-    Dataset(name="G4ABLA", version="3.3", filename="G4ABLA", env="G4ABLADATA"),
-    Dataset(name="G4INCL", version="1.2", filename="G4INCL", env="G4INCLDATA"),
+    Dataset(
+        name="G4ABLA",
+        version="3.3",
+        filename="G4ABLA",
+        env="G4ABLADATA",
+        md5sum="b25d093339e1e4532e31038653580ca6",
+    ),
+    Dataset(
+        name="G4INCL",
+        version="1.2",
+        filename="G4INCL",
+        env="G4INCLDATA",
+        md5sum="0a76df936839bb557dae7254117eb58e",
+    ),
     Dataset(
         name="G4ENSDFSTATE",
         version="2.3",
         filename="G4ENSDFSTATE",
         env="G4ENSDFSTATEDATA",
+        md5sum="6f18fce8f217e7aaeaa3711be9b2c7bf",
     ),
 )
 
@@ -104,6 +149,16 @@ def _download_extract_dataset(dataset: Dataset, pbar: tqdm):
             pbar.update(chunk_size)
 
         f.seek(0)
+        md5 = hashlib.md5()
+        for chunk in iter(lambda: f.read(chunk_size), b""):
+            md5.update(chunk)
+        if md5.hexdigest() != dataset.md5sum:
+            print(
+                f"MD5 checksum mismatch for {filename}: got {md5.hexdigest()} but expected {dataset.md5sum}"
+            )
+            raise RuntimeError(f"MD5 checksum mismatch for {filename}")
+
+        f.seek(0)
         with tarfile.open(fileobj=f, mode="r:gz") as tar:
             tar.extractall(data_directory())
 
@@ -120,27 +175,32 @@ def missing_datasets(directory: str | None = None) -> list[Dataset]:
     return datasets_to_download
 
 
-def check_datasets() -> bool:
+def check_datasets(throw: bool = False) -> bool:
     datasets_to_download = missing_datasets()
     if datasets_to_download:
-        print(
-            f"Missing Geant4 datasets: {', '.join([f'{dataset.name}@v{dataset.version}' for dataset in datasets_to_download])}"
-        )
+        if throw:
+            raise RuntimeError(
+                f"Missing Geant4 datasets: {', '.join([f'{dataset.name}@v{dataset.version}' for dataset in datasets_to_download])}"
+            )
         return False
     return True
 
 
 def install_datasets(show_progress: bool = True):
-    # first try to see if the datasets are already installed in the Geant4 data directory
-    datasets_to_download = missing_datasets(os.environ["GEANT4_DATA_DIR"])
-    if not datasets_to_download:
-        return
-
-    os.environ["GEANT4_DATA_DIR"] = data_directory()
+    # first try to see if the datasets are installed in the application directory
     datasets_to_download = missing_datasets()
-
-    if datasets_to_download != 0:
+    if not datasets_to_download:
+        # datasets are installed in application directory
+        os.environ["GEANT4_DATA_DIR"] = data_directory()
         return
+
+    # check if the datasets are present in the corresponding Geant4 directory
+    if not bool(missing_datasets(os.environ["GEANT4_DATA_DIR"])):
+        # return
+        ...
+
+    # download and extract the datasets
+    os.environ["GEANT4_DATA_DIR"] = data_directory()
 
     os.makedirs(data_directory(), exist_ok=True)
     if show_progress:
