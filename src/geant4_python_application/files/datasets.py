00c37aaf7052518f10fbc2d262bd91097f0c2cff
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-10-09 13:23:46 +0200

    fix bad getter for geant4 data path

--- src/geant4_python_application/files/datasets.py
+++ src/geant4_python_application/files/datasets.py
@@ -33,10 +33,16 @@ def application_data_directory() -> str:
 
 def geant4_data_directory() -> str:
     try:
-        geant4_prefix = subprocess.run(["geant4-config", "--prefix"], check=True)
-        return os.path.join(
+        # capture stdout, save into a str
+        geant4_prefix = subprocess.run(["geant4-config", "--prefix"], capture_output=True)
+        geant4_data_path = os.path.join(
             geant4_prefix.stdout.decode().strip(), "share", "Geant4", "data"
         )
+        if not os.path.exists(geant4_data_path):
+            return ""
+
+        return geant4_data_path
+
     except subprocess.CalledProcessError:
         return ""
 
@@ -138,7 +144,7 @@ def _get_dataset_download_size(dataset: Dataset) -> int:
 
 def _get_total_download_size(datasets_to_download: list[Dataset] = datasets) -> int:
     with concurrent.futures.ThreadPoolExecutor(
-        max_workers=len(datasets_to_download)
+            max_workers=len(datasets_to_download)
     ) as executor:
         futures = [
             executor.submit(_get_dataset_download_size, dataset)
@@ -228,14 +234,14 @@ The following Geant4 datasets will be installed: {", ".join([f"{dataset.name}@v{
         )
 
     with tqdm(
-        total=_get_total_download_size(datasets_to_download),
-        desc="Downloading Geant4 datasets",
-        disable=not show_progress,
-        unit="B",
-        unit_scale=True,
+            total=_get_total_download_size(datasets_to_download),
+            desc="Downloading Geant4 datasets",
+            disable=not show_progress,
+            unit="B",
+            unit_scale=True,
     ) as pbar:
         with concurrent.futures.ThreadPoolExecutor(
-            max_workers=len(datasets_to_download)
+                max_workers=len(datasets_to_download)
         ) as executor:
             futures = [
                 executor.submit(_download_extract_dataset, dataset, pbar)
@@ -246,7 +252,7 @@ The following Geant4 datasets will be installed: {", ".join([f"{dataset.name}@v{
     if show_progress:
         total_size_gb = sum(
             fp.stat().st_size for fp in Path(application_data_directory()).rglob("*")
-        ) / (1024**3)
+        ) / (1024 ** 3)
         print(f"Geant4 datasets size on disk after extraction: {total_size_gb:.2f}GB")
 
 
